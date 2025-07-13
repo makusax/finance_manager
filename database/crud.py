87000a036579b28ec453acb2baefb7fcb2597832
makusax <4ndrey2016@gmail.com>
2025-07-13 05:54:25 +0300

    Commit

---
+++ database/crud.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+def add_transaction(amount: float, category: str, description: str, type: str):
+    conn = sqlite3.connect("finance.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO transactions (amount, category, description, type) VALUES (?, ?, ?, ?)",
+                   (amount, category, description, type))
+    conn.commit()
+    conn.close()
