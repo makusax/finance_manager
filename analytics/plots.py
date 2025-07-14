89551fecb143332e9c517ea690745336a813b3f3
makusax <4ndrey2016@gmail.com>
2025-07-14 11:15:11 +0300

    Commit

--- analytics/plots.py
+++ analytics/plots.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 from database.crud import get_all_transactions
 
+
 def show_spending_pie():
     transactions = get_all_transactions()
-    # ... логика группировки по категориям
-    plt.pie(category_totals, labels=labels, autopct='%1.1f%%')
-    plt.title("Расходы по категориям")
+    expenses = [t for t in transactions if t[5] == 'expense']
+
+    categories = {}
+    for _, amount, category, _, _, _ in expenses:
+        categories[category] = categories.get(category, 0) + amount
+
+    plt.pie(categories.values(), labels=categories.keys(), autopct='%1.1f%%')
+    plt.title("Распределение расходов")
     plt.show()
